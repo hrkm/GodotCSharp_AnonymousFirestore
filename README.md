120d16f09f6547d7371031880e9c13148e6dff78
Mateusz Cichenski <mcichenski@gmail.com>
2025-07-05 14:48:32 -0700

    Adding basic support for Google Analytics!

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This library is meant to be used for a quick and easy simple anonymous data collection with Firebase in Godot.
 The library supports anonymous authentication, that can be then used to create documents in Firestore.
+The library also has a simple Google Analytics implementation for sending basic event data.
 It is using the REST APIs of the Firebase.Authentication and Cloud.Firestore functions.
 It is also using the Godot FileAccess/DirAccess classes to store the authentication data in a local encrypted file.
 
@@ -48,6 +49,35 @@ This will only allow a given authenticated user the ability to write a document 
 
 Please note that you can set a quota of how many users can be created from a given IP address in Firebase.Authentication Settings in the Firebase console.
 
+#### Using Google Analytics
+Download the repository and copy into your Godot project.
+
+Somewhere in your code initialize Google Analytics logic like this:
+```
+var analytics = new Firebase.Analytics(<analytics_secret_api_key>, <measurement_id>, <app_instance_id>);
+analytics.UserId = <user_id>; // optional, allows to differentiate the users and provide counts of active users
+analytics.SendScreenViewEvent("MyScene");
+analytics.SendEvent(new Analytics.Event("my custom event", new Dictionary<string, object>() { { "param1", 23 } }));
+```
+
+#### Where do I get the different keys and values for the configuration?
+
+<your_api_key> - API key from the Firebase, open Project Settings and find your app in the list, there should be a configuration section with your apiKey and projectId
+
+<your_project_id> - the id of the project as it appears in https://console.firebase.google.com/project/{project-id} (or in the config mentioned above)
+
+<your_collection_id> - any name for the collection you want, just a string
+
+<measurement_id> - this is from Google Analytics, go to Admin > Data Streams -> (select your stream) -> the id should be in the upper right corner, starts with "G-"
+
+<analytics_secret_api_key> - this is also in Data Streams section, after selecting a stream you have to open "Measurement Protocol API secrets" and generate a new secret
+
+<app_instance_id> - it's supposed to be 32 character long hexadecimal string, "00000000000000000000000000000000" works just fine? :)
+
+<user_id> - if you are using the Firebase.Authentication, you can use that to disambiguate the Google Analytics users
+
+
+
 #### Why didn't you implement X from Firebase?
 
 This is meant to be used as a way to upload anonymous data from players gameplay. It's not meant to be used to maintain the player profile data (e.g. what they unlocked in the game or what are their stats), as that is something that you should be using OAuth methods for and be able to update the documents, which neither is provided in this implementation.
@@ -58,7 +88,7 @@ Since you are providing the data in FirestoreDocument, it is your responsibility
 
 #### What are the future plans?
 
-There should be a better way of doing the JSON formatted classes for easier data manipulation than what I have here. There is also no error handling right now (it will just throw exception) - I might add a wrapper around it and include events to listen to in case a failure occurs.
+There is also no error handling right now (it will just throw exception) - I might add a wrapper around it and include events to listen to in case a failure occurs. I may consider adding more of the standard Analytics events depending on the results.
 
 #### This is awesome! How can I support you?
 
---
+++ addons/godotcsharp_anonymousfirestore/Analytics.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Firebase
+{
+	public class Analytics
+	{
+		public event EventHandler AnalyticsSent;
+		public event EventHandler AnalyticsFailed;
+
+		private const string BASE_URL = "https://www.google-analytics.com";
+		private string MEASUREMENT_PROTOCOL_COLLECT_URL = "/mp/collect";
+
+		private string _apiSecret;
+		private string _measurementId;
+		private string _appInstanceId;
+		public string UserId;
+		public Analytics(string apiSecret, string measurementId, string appInstanceId)
+		{
+			_apiSecret = apiSecret;
+			_measurementId = measurementId;
+			_appInstanceId = appInstanceId;
+		}
+
+		private HttpClient _analyticsClient = new HttpClient()
+		{
+			BaseAddress = new Uri(BASE_URL)
+		};
+
+		/// <summary>
+		/// Request body sent to Google Analytics.
+		/// </summary>
+		/// <param name="AppInstanceId">app instance id, should be unique per app instance</param>
+		/// <param name="UserId">optional user id, if provided enables active users reports</param>
+		/// <param name="Events">list of events to upload, as per the API the limit is 25 per request</param>
+		public record class AnalyticsRequestBody(
+			[property:JsonPropertyName("app_instance_id")]
+			string AppInstanceId,
+			[property:JsonPropertyName("user_id")]
+			[property:JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+			string UserId,
+			[property:JsonPropertyName("events")]
+			List<Event> Events);
+
+		public record class Event(
+			[property:JsonPropertyName("name")]
+			string Name,
+			[property:JsonPropertyName("params")]
+			Dictionary<string, object> Params);
+
+		/// <summary>
+		/// Sends a screen_view analytics event.
+		/// </summary>
+		/// <param name="screenName">name of the screen that is viewed</param>
+		/// <returns>true, if event was sent successfully</returns>
+		public async Task<bool> SendScreenViewEvent(string screenName)
+		{
+			var data = new Event("screen_view", new Dictionary<string, object>()
+				{
+					{ "screen_name", screenName },
+					{ "engagement_time_msec", 1 } // this enables to count active users when combined with UserId
+				});
+			return await SendEvent(data);
+		}
+
+		/// <summary>
+		/// Sends an arbitrary event to the Google Analytics.
+		/// </summary>
+		/// <param name="data">event to be sent</param>
+		/// /// <returns>true, if event was sent successfully</returns>
+		public async Task<bool> SendEvent(Event data) {
+			return await SendEvents(new List<Event>() { data });
+		}
+
+		/// <summary>
+		/// Sends a list of events to Google Analytics
+		/// </summary>
+		/// <param name="data">list of events to upload in single query, limit is 25!</param>
+		/// <returns>true, if events were sent successfully</returns>
+		public async Task<bool> SendEvents(List<Event> data)
+		{
+			if (data.Count > 25)
+			{
+				AnalyticsFailed?.Invoke(this, EventArgs.Empty);
+				return false;
+			}
+			using StringContent content = new(JsonSerializer.Serialize(new AnalyticsRequestBody(_appInstanceId, UserId, data)));
+			using var response = await _analyticsClient.PostAsync(MEASUREMENT_PROTOCOL_COLLECT_URL + "?api_secret=" + _apiSecret + "&measurement_id=" + _measurementId, content);
+
+			if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+			{
+				AnalyticsFailed?.Invoke(this, EventArgs.Empty);
+				return false;
+			}
+			AnalyticsSent?.Invoke(this, EventArgs.Empty);
+			return true;
+		}
+	}
+}
